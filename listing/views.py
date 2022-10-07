8d3b8b2368e12ab421782561d49e49351695858b
Haris <harisleghari61@gmail.com>
2022-10-07 20:17:29 +0500

    create logicbase

---
+++ listing/views.py
@@ -0,0 +1,54 @@
+from multiprocessing import context
+import re
+from django.shortcuts import render, redirect
+
+from .models import Listing
+from .forms import ListingForm
+
+# Create your views here.
+
+#CRUD create , retrieve, update, delete, and also a list
+
+# 1-list
+def listing_list(request):
+    listings = Listing.objects.all()
+    context = {
+        "listings": listings
+    }
+
+    return render(request, "index.html", context)
+
+# 2-retrieve
+def listing_retrive(request, pk):
+    listing = Listing.objects.get(id= pk)
+    context = {
+        "listing": listing
+    }
+    return render(request, "retrieve.html", context)
+
+# 3-create
+def listing_create(request):
+    form = ListingForm()
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    context = {
+        "form": form
+    }
+    return render(request, "create.html", context)
+
+# 3-update
+def listing_update(request, pk):
+    listing = Listing.objects.get(id=pk)
+    form = ListingForm()
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    context = {
+        "form": form
+    }
+    return render(request, "create.html", context)
